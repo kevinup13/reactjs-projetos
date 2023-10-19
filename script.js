7070a5a81594144867749c60c1834b0dafb1da27
kevin <kevinup13@gmail.com>
2023-10-19 16:37:31 -0400

    adding function in js file

--- script.js
+++ script.js
@@ -4,9 +4,31 @@ alert("Bem vindo(a)!");
 
 let nome = prompt("Digite seu nome: ");
 
+function formataNome() {
+    let letra;
+    let tamanho;
+    tamanho = nome.length;
+    for (let i = 0; i < tamanho; i++) {
+        letra = nome.charAt(i);
+        if (letra == ' ') {
+            if ((i + 1) < tamanho) {
+                letra = nome.charAt(i + 1).toUpperCase();
+                nome = nome.substring(0, i + 1);
+                nome += letra;
+            }
+        }
+        if (tamanho > 0) {
+            letra = nome.charAt(0).toUpperCase();
+            nome = nome.substring(1, tamanho);
+            nome = letra + nome;
+        }
+    }
+}
+
 if (nome == '') {
     nome = "Sem Nome";
     document.querySelector(".name").innerHTML = `Olá ${nome}, seja bem vindo de volta!`;
 } else {
+    formataNome(nome);
     document.querySelector(".name").innerHTML = `Olá ${nome}, seja bem vindo de volta!`;
 }
