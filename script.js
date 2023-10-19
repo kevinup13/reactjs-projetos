c4c258fce5f7ed58a40ced4bbb50427af58125cb
kevin <kevinup13@gmail.com>
2023-10-19 15:53:49 -0400

    update events js file

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-alert("Hello world!");
 
-let nome = prompt("Digite seu nome: ")
+// Initial data
+alert("Bem vindo(a)!");
 
-document.querySelector(".name").innerHTML = `Olá ${nome}, seja bem vindo de volta!`;
+let nome = prompt("Digite seu nome: ");
+
+if (nome == '') {
+    nome = "Sem Nome";
+    document.querySelector(".name").innerHTML = `Olá ${nome}, seja bem vindo de volta!`;
+} else {
+    document.querySelector(".name").innerHTML = `Olá ${nome}, seja bem vindo de volta!`;
+}
